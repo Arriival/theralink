9c44aa1d819d1eb39a13efea0599c34a4acec91c
m.lami <Mohammad.lami57@gmail.com>
2023-01-25 15:19:19 +0330

    init

---
+++ src/main/java/com/domain/Consultant.java
@@ -0,0 +1,7 @@
+package com.domain;
+
+import com.core.framework.domain.person.Person;
+
+public class Consultant {
+    private Person person;
+}
---
+++ src/main/java/com/domain/ConsultantType.java
@@ -0,0 +1,19 @@
+package com.domain;
+
+import com.core.framework.domain.BaseEntity;
+import com.core.framework.domain.baseInformation.BaseInformation;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+//@Table(name = "MAC_CONSULTANT_TYPE")
+//@GenericGenerator(name = "sequence_db", strategy = "org.hibernate.id.UUIDGenerator")
+public class ConsultantType extends BaseEntity<String> {
+    @ManyToOne
+    @JoinColumn(name = "EDUCATION_LEVEL_ID")
+    private BaseInformation educationLevel;
+
+    @Column(name = "UNIT")
+    private Double unit;
+}
